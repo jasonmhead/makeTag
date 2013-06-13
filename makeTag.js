66a40bcac171d174560e5701d02a183fc5b50527
namelgroup <jmhead@namelgroup.com>
2013-06-12 23:11:55 -0300

    Fixes

--- makeTag.js
+++ makeTag.js
@@ -1,15 +1,15 @@
 function makeTag(tag,tContent,tID,tClass,tValue,tAlt,tOther){
-	var tagAttributeNames = [];
+	var tagAttributeNames = ['','','id','class','value','alt'];
 	var tagAttributes, tagOutput;
-	
-	tagAttributeNames = ['','','id','class','value','alt'];
-	
+        tagAttributes = '';
+
 		for(var i=0; i<arguments.length; i++) {
 			if(i > 1 && i < 6 && arguments[i] != undefined && arguments[i] != ''){
-				tagAttributes += tagAttributeNames+' "'+arguments[i]+'" ';
+				tagAttributes += tagAttributeNames[i]+'="'+arguments[i]+'" ';
+alert(i+' :: '+tagAttributeNames[i]+' :: '+arguments[i]);
 			}
   		}
-	
+
 		if(tContent == undefined || tContent == ''){ // is this tag self-closing?
 			tagOutput = '<'+tag+' '+tagAttributes+' />';
 		}else{
